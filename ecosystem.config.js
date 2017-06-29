89b43f5c33013bdc381c281ce61438ab1e00f8e8
zenxds <zenxds@gmail.com>
2017-06-29 14:23:23 +0800

    调整部署

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,23 +4,9 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
   apps : [
-
-    // First application
     {
-      name      : 'API',
-      script    : 'app.js',
-      env: {
-        COMMON_VARIABLE: 'true'
-      },
-      env_production : {
-        NODE_ENV: 'production'
-      }
-    },
-
-    // Second application
-    {
-      name      : 'WEB',
-      script    : 'web.js'
+      name      : 'koa-seed',
+      script    : 'index.js'
     }
   ],
 
