ac66207782f405faf0493ced0670d1d5a9be3b91
zenxds <zenxds@gmail.com>
2017-09-27 10:48:31 +0800

    success

--- app/middleware/success.js
+++ app/middleware/success.js
@@ -6,10 +6,14 @@ const isJSON = require('koa-is-json')
 module.exports = async function(ctx, next) {
   await next()
 
-  if (isJSON(ctx.body)) {
+  if (isJSON(ctx.body) && ctx.status === 200 && !hasWrapped(ctx.body)) {
     ctx.body = {
       success: true,
       data: ctx.body
     }
   }
+}
+
+function hasWrapped(data) {
+  return typeof data === 'object' && typeof data.success !== 'undefined'
 }
