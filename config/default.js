b3a64580e418d9f5da5a3277a0eb092dca121301
zenxds <zenxds@gmail.com>
2018-07-24 13:19:18 +0800

    update

--- config/default.js
+++ config/default.js
@@ -2,12 +2,6 @@ module.exports = {
   keys: ['58f9014fb686fe9b6449f1769e37ec90a676e9c6'],
   port: 7002,
 
-  mail: {
-    from: '',
-    user: '',
-    password: ''
-  },
-
   db: {
     dialect: "mysql",
     host: "127.0.0.1",
