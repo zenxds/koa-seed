24b34df15ab07d04e684d8122181ef3416c2e13d
zenxds <zenxds@gmail.com>
2019-01-11 15:18:36 +0800

    eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,58 @@
 module.exports = {
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 8
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaVersion": 8
   },
-  env: {
-    node: true,
-    jest: true
+  "env": {
+    "node": true,
+    "jest": true
   },
-  extends: ['eslint:recommended'],
-  globals: {
-    Promise: true,
-    Symbol: true,
-    Proxy: true
+  "extends": [
+    "eslint:recommended"
+  ],
+  "globals": {
+    "Promise": true,
+    "Symbol": true,
+    "Proxy": true
   },
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['warn', 'single'],
-    'no-unused-vars': 'warn',
-    'no-console': [
-      'error',
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "warn",
+      "single"
+    ],
+    "no-unused-vars": "warn",
+    "no-console": [
+      "error",
       {
-        allow: ['log', 'warn', 'error']
+        "allow": [
+          "log",
+          "warn",
+          "error"
+        ]
       }
     ],
-    'no-empty': [
-      'error',
+    "no-empty": [
+      "error",
       {
-        allowEmptyCatch: true
+        "allowEmptyCatch": true
       }
     ],
-    semi: ['warn', 'never'],
-    'eol-last': 'warn',
-    'comma-dangle': ['error', 'never']
+    "semi": [
+      "warn",
+      "never"
+    ],
+    "eol-last": "warn",
+    "comma-dangle": [
+      "error",
+      "never"
+    ]
   }
 }
